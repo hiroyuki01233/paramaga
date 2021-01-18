df27cc1ecfc5955cb248cf831fabf34ee0dcb644
勝田博之 <katsutahiroyuki@katsutahiroyukinoMacBook-Pro.local>
2021-01-18 20:22:42 +0900

    profile photo

--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -70,11 +70,11 @@
                                     {{ __('Profile') }}
                                 </x-jet-dropdown-link>
 
-                                @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
+                                {{-- @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
                                     <x-jet-dropdown-link href="{{ route('api-tokens.index') }}">
                                         {{ __('API Tokens') }}
                                     </x-jet-dropdown-link>
-                                @endif
+                                @endif --}}
 
                                 <div class="border-t border-gray-100"></div>
 
@@ -166,11 +166,11 @@
                                         {{ __('Profile') }}
                                     </x-jet-responsive-nav-link>
     
-                                    @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
+                                    {{-- @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
                                         <x-jet-responsive-nav-link href="{{ route('api-tokens.index') }}" :active="request()->routeIs('api-tokens.index')">
                                             {{ __('API Tokens') }}
                                         </x-jet-responsive-nav-link>
-                                    @endif
+                                    @endif --}}
     
                                     <!-- Authentication -->
                                     <form method="POST" action="{{ route('logout') }}">
