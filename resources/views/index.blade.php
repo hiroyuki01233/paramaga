8a38af44dafb82a89c90175010432190a4f0a1db
勝田博之 <katsutahiroyuki@katsutahiroyukinoMacBook-Pro.local>
2021-01-20 18:06:57 +0900

    duble cuoute

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ ”パラパラ漫画を作成して共有！Paramaga / パラマガ” }}</title>
+        <title>{{ "パラパラ漫画を作成して共有！Paramaga / パラマガ" }}</title>
         <meta name=”description” content=”パラマガでパラパラ漫画を作ろう。絵が得意な方！パラパラ漫画を試しに作ってみたい方！ウェブサイト上で手軽にパラパラ漫画を作成してたくさんの人共有しましょう！あなたの素敵な作品をみたたくさんの人からいいねやコメントがもらえるかも！“>
 
         <!-- Fonts -->
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ ”パラパラ漫画を作成して共有！Paramaga / パラマガ” }}</title>
+        <title>{{ "パラパラ漫画を作成して共有！Paramaga / パラマガ" }}</title>
         <meta name=”description” content=”絵が得意な方！パラパラ漫画を試しに作ってみたい方！ウェブサイト上で手軽にパラパラ漫画を作成してそれを公開、たくさんの人と共有しましょう！あなたの素敵な作品をたくさんの人に閲覧してもらいましょう。あなたの素敵な作品をみた人からいいねやコメントがもらえるかも！“>
 
         <!-- Fonts -->
