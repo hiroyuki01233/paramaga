6d54c9ee6b15c3f15b7d4c5d69509750e7fdbad5
勝田博之 <katsutahiroyuki@katsutahiroyukinoMacBook-Pro.local>
2021-01-28 16:31:25 +0900

    forget pass jp

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -5,7 +5,7 @@
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+            {{ __('パスワードをお忘れですか？大丈夫です。メールアドレスをお知らせください。新しいパスワードを選択できるパスワード再設定リンクをメールでお送りします。') }}
         </div>
 
         @if (session('status'))
@@ -26,7 +26,7 @@
 
             <div class="flex items-center justify-end mt-4">
                 <x-jet-button>
-                    {{ __('Email Password Reset Link') }}
+                    {{ __('パスワード再設定リンク') }}
                 </x-jet-button>
             </div>
         </form>
