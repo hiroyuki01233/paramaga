6e3bcae91d1e3ac96333b6835d2983ec40225c39
勝田博之 <katsutahiroyuki@katsutahiroyukinoMacBook-Pro.local>
2021-01-28 22:04:26 +0900

    add google analytics

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3W2D6ZB0MX"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-3W2D6ZB0MX');
+        </script>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -39,7 +48,6 @@
             }
 
         </style>
-
         <script>
             function setCSRF(){
                 $.ajaxPrefilter(function (options, originalOptions, jqXHR) {
